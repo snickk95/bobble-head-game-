76d54b491c1e0788b9f184fec124e283943e9830
snick harrison <harrisonnick16@gmai.com>
2019-09-15 00:03:27 -0400

    add camera follow

---
+++ Assets/Scripts/cameraMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovment : MonoBehaviour
+{
+    public GameObject followTarget;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //fail safe for if the player is not detected
+        if (followTarget != null)
+        {
+            transform.position = Vector3.Lerp(transform.position,
+            followTarget.transform.position, Time.deltaTime * moveSpeed);
+
+        }
+
+
+    }
+}
