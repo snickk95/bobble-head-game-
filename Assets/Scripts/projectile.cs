161bf446c7502d2f84c5b6b450235d19e16076ba
snick harrison <harrisonnick16@gmai.com>
2019-09-15 01:37:05 -0400

    add script to make bullets disapear

---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //destroy the object when no longer on camera
+    private void OnBecomeInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    //destroy when the object hits the alien
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
