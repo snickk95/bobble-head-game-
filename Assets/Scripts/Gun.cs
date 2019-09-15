08661a5d14cd7b214049c898a9890a3d43759cff
snick harrison <harrisonnick16@gmai.com>
2019-09-15 00:25:44 -0400

    add firing projectile from gun

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public Transform launchPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void fireBullet()
+    {
+        // creates a game object thats a  existing prefab 
+        GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+
+
+        // sets the position of the object to where the gun is 
+        bullet.transform.position = launchPosition.position;
+
+
+        // atatch it to the rigidbody of the marine so it fires in the same direction the marine is facing
+        bullet.GetComponent<Rigidbody>().velocity = transform.parent.forward * 100;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check if mouse button pressed to call firing the bullet
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!IsInvoking("fireBullet"))
+            {
+                InvokeRepeating("fireBullet", 0f, 0.1f);
+            }
+        }
+
+        // stops the bullet from firing again when you let go of mouse button
+        if (Input.GetMouseButtonUp(0))
+        {
+            CancelInvoke("fireBullet");
+        }
+
+    }
+}
