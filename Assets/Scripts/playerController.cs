b9b8de4e8fc52688ac411d88f5b9d4e6d7f937aa
Nicholas Harrison [Student] <nicholas.harrison@student.sl.on.ca>
2019-09-16 16:21:06 -0400

    end of class sept 16 page 123

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -7,7 +7,8 @@ public class playerController : MonoBehaviour
     //public float for move speed can be changed in unity editor
     public float moveSpeed = 50.0f;
     public Rigidbody head;
-    
+    public LayerMask layerMask;
+    private Vector3 currentLookTarget = Vector3.zero;
 
     //variable to contain the character controller
     private CharacterController characterController;
@@ -42,6 +43,20 @@ public class playerController : MonoBehaviour
         else
         {
             head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
+        
+        RaycastHit hit;
+        //sending the ray from the main camera to the mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //this just shows you the ray well playing the game
+        Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
+
+        // physics. ray cast makes the ray then checks if it hit 1000 is how long the ray is the layer mask shows what the player is trying to hit 
+        //the ignore tells the physics engine not to activate triggers
+        if (Physics.Raycast(ray, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+        {
+
         }
 
     }
