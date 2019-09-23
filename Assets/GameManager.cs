7ef07e32e5671ad992170d0b80e0c94924f61d1c
Nicholas Harrison [Student] <nicholas.harrison@student.sl.on.ca>
2019-09-23 15:49:12 -0400

    making aliens spawn

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -28,6 +28,56 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentSpawnTime += Time.deltaTime;    
+        //
+        if (currentSpawnTime> generatedSpawnTime)
+        {
+            currentSpawnTime = 0;
+        }
+
+        generatedSpawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        //
+        if (aliensPerSpawn > 0 && aliensOnScreen < totalAliens)
+        {
+            List<int> previousSpawnLocations = new List<int>();
+
+            if (aliensPerSpawn > spawnPoints.Length)
+            {
+                aliensPerSpawn = spawnPoints.Length - 1;
+            }
+
+            //
+            aliensPerSpawn = (aliensPerSpawn > totalAliens) ? aliensPerSpawn - totalAliens : aliensPerSpawn;
+
+            //
+            for (int i = 0; i < aliensPerSpawn; i++)
+            {
+                if (aliensOnScreen < maxAliensOnScreen)
+                {
+                    aliensOnScreen += 1;
+
+                    int spawnPoint = -1;
+
+                    while (spawnPoint == -1)
+                    {
+                        int randomNumber = Random.Range(0, spawnPoints.Length - 1);
+
+                        if (!previousSpawnLocations.Contains(randomNumber))
+                        {
+                            previousSpawnLocations.Add(randomNumber);
+
+                            spawnPoint = randomNumber;
+                        }
+                    }
+
+                    GameObject spawnLocation = spawnPoints[spawnPoint];
+                    GameObject newAlien = Instantiate(alien) as GameObject;
+                    newAlien.transform.position = spawnLocation.transform.position;
+                }
+            }
+        }
+        //
         
+
     }
 }
