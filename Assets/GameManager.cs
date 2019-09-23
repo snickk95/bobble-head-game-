c9d661d20bad581e990829600981bc2b77f13baf
Nicholas Harrison [Student] <nicholas.harrison@student.sl.on.ca>
2019-09-23 15:08:56 -0400

    added a game manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //public variables
+    public GameObject player;
+    public GameObject[] spawnPoints;
+    public GameObject alien;
+    public int maxAliensOnScreen;
+    public int totalAliens;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+    public int aliensPerSpawn;
+
+    //private variables
+    private int aliensOnScreen = 0;
+    private float generatedSpawnTime = 0;
+    private float currentSpawnTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
