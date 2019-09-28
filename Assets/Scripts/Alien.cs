911e9ff1f0fc778a423fd2223fbbfac457020b09
snick harrison <harrisonnick16@gmai.com>
2019-09-28 13:44:54 -0400

    making aliens move towards player

---
+++ Assets/Scripts/Alien.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Alien : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //refrence to nav mesh to access in code
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.destination = target.position;
+        }
+    }
+}
