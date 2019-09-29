15b6c1491daff30d6d466219a4933f4b78adac44
snick harrison <harrisonnick16@gmai.com>
2019-09-29 14:52:53 -0400

    add sound manager script

---
+++ Assets/soundManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    public static soundManager Instance = null;
+
+    private AudioSource soundEffectAudio;
+
+    //sound variables
+    public AudioClip gunFire;
+
+    public AudioClip upgradedGunFire;
+
+    public AudioClip marineDeath;
+
+    public AudioClip hurt;
+
+    public AudioClip alienDeath;
+   
+    public AudioClip victory;
+
+    public AudioClip elevatorArrived;
+
+    public AudioClip powerUpPickup;
+
+    public AudioClip powerUpAppear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //initalizes an array to get the audio soruce and gets a frefrence to that clip
+        AudioSource[] sources = GetComponents<AudioSource>();
+
+        foreach (AudioSource source in sources)
+        {
+            if (source.clip == null)
+            {
+                soundEffectAudio = source;
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
