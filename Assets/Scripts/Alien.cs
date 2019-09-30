87321d1a1391f69f415806266ca1c5d7f0069fd5
snick harrison <harrisonnick16@gmai.com>
2019-09-29 20:54:24 -0400

    decapitating the alien

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -10,6 +10,8 @@ public class Alien : MonoBehaviour
     private NavMeshAgent agent;
     public UnityEvent OnDestroy;
     public float navigationUpdate;
+    public Rigidbody head;
+    public bool isAlive = true;
     private float navigationTime=0;
 
     // Start is called before the first frame update
@@ -22,17 +24,20 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        if (isAlive)
         {
-            // old way
-            // agent.destination = target.position;
-
-            //new way
-            navigationTime += Time.deltaTime;
-            if (navigationTime > navigationUpdate)
+            if (target != null)
             {
-                agent.destination = target.position;
-                navigationTime = 0;
+                // old way
+                // agent.destination = target.position;
+
+                //new way
+                navigationTime += Time.deltaTime;
+                if (navigationTime > navigationUpdate)
+                {
+                    agent.destination = target.position;
+                    navigationTime = 0;
+                }
             }
         }
     }
@@ -43,13 +48,30 @@ public class Alien : MonoBehaviour
 
         OnDestroy.RemoveAllListeners();
 
+        head.GetComponent<selfDestruct>().Initiate();
+
         Destroy(gameObject);
     }
 
+    public void die()
+    {
+        isAlive = false;
+        head.GetComponent<Animator>().enabled = false;
+        head.isKinematic = false;
+        head.useGravity = true;
+        head.GetComponent<SphereCollider>().enabled = true;
+        head.gameObject.transform.parent = null;
+        head.velocity = new Vector3(0, 26.0f, 3.0f);
+        OnDestroy.Invoke();
+        OnDestroy.RemoveAllListeners();
+        soundManager.Instance.PlayOneShot(soundManager.Instance.alienDeath);
+        Destroy(gameObject);
+    }
 
     void OnTriggerEnter(Collider other)
     {
         Die();
         soundManager.Instance.PlayOneShot(soundManager.Instance.alienDeath);
     }
+
 }
---
+++ Assets/selfDestruct.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfDestruct : MonoBehaviour
+{
+    public float destructTime = 3.0f;
+    public void Initiate()
+    {
+        Invoke("selfDestruct", destructTime);
+    }
+    private void SelfDestruct()
+    {
+        Destroy(gameObject);
+    }
+}
